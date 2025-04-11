ca255f7d949669986c4e6df6f55153426537a348
RamyaHarika-Dadi <harika.ramya.dadi@gmail.com>
2025-04-10 21:45:31 -0400

    Update 1_Database_Exploration.sql

--- scripts/Exploratory Data Analysis/1_Database_Exploration.sql
+++ scripts/Exploratory Data Analysis/1_Database_Exploration.sql
@@ -1,9 +1,23 @@
+/*
+===============================================================================
+Database Exploration
+===============================================================================
+Purpose:
+    - To explore the structure of the database, including the list of tables and their schemas.
+    - To inspect the columns and metadata for specific tables.
+
+Tables Used:
+    - INFORMATION_SCHEMA.TABLES
+    - INFORMATION_SCHEMA.COLUMNS
+===============================================================================
+*/
+
 -- Explore all objects in the Database
 SELECT * FROM INFORMATION_SCHEMA.TABLES;
 
 -- Explore all the columns in the Database
 SELECT * FROM INFORMATION_SCHEMA.COLUMNS;
 
--- Explore columns of specific table
+-- Explore columns of a specific table
 SELECT * FROM INFORMATION_SCHEMA.COLUMNS
 WHERE TABLE_NAME = 'dim_customers';
